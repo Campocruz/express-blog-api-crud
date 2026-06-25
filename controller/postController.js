85839bc308aa1f944610e00cbae7123c48cd0b64
Campocruz <cosmacampo93@yahoo.it>
2026-06-26 00:41:30 +0200

    add in a show function to show specic id into db

--- controller/postController.js
+++ controller/postController.js
@@ -18,13 +18,15 @@ const index = (req, res) => {
 }
 
 const show = (req, res) => {
-  const index = posts.findIndex(post => post.id === parseInt(req.params.id))
-  if (index === -1) {
-    return res.status(404).json({
-      error: 'Post non trovato'
-    })
+  if (req.params.id) {
+    const id = Number(req.params.id);
+    const sql = `SELECT * FROM posts WHERE id = ?`;
+    connection.query(sql, [id], (err, results) => {
+      if (err) return res.status(500).json({ error: 'Database query failed' })
+      if (results.length === 0) return res.status(404).json({ error: 'Post not found' });
+      res.json(results[0])
+    });
   }
-  res.json(posts[index])
 }
 
 const store = (req, res) => {
@@ -103,7 +105,7 @@ const modify = (req, res) => {
 
 const destroy = (req, res) => {
   if (req.params.id) {
-    const id = parseInt(req.params.id);
+    const id = Number(req.params.id);
     const findIdSql = `SELECT * FROM posts WHERE id = ?`;
     connection.query(findIdSql, [id], (err, results) => {
       if (err) return res.status(500).json({ error: 'Database query failed' })
